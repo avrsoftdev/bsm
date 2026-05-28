d0139abdbad6fba01b6ef50a2c06f0dce04b3830
Aakash Dodwal <naukariwalaav@gmail.com>
2026-05-28 14:29:08 +0530

    Fix Flutter build script: use curl instead of wget and improve PATH setup

--- build.sh
+++ build.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
+set -e
 
 # Download and install Flutter
 echo "Installing Flutter..."
-wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.22.0-stable.tar.xz
-tar -xf flutter_linux_3.22.0-stable.tar.xz
-export PATH=$PATH:$(pwd)/flutter/bin
+FLUTTER_URL="https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.22.0-stable.tar.xz"
+curl -L $FLUTTER_URL -o flutter.tar.xz
+tar -xf flutter.tar.xz
+rm flutter.tar.xz
+
+# Add Flutter to PATH
+export PATH="$PATH:$(pwd)/flutter/bin"
+export FLUTTER_HOME="$(pwd)/flutter"
+
+# Verify Flutter installation
+flutter --version
+dart --version
 
 # Install dependencies
 echo "Getting Flutter dependencies..."
