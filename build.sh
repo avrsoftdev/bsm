d65932d2372e86fa02130e7e2a961b9a309f6140
Aakash Dodwal <naukariwalaav@gmail.com>
2026-05-28 14:45:37 +0530

    Fix Flutter and Dart SDK versions for Vercel deployment

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ set -e
 # Fix git dubious ownership issue
 git config --global --add safe.directory '*'
 
-# Download and install Flutter
+# Download and install Flutter 3.29.0 (has Dart 3.9.0+)
 echo "Installing Flutter..."
-FLUTTER_URL="https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.22.0-stable.tar.xz"
+FLUTTER_URL="https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.29.0-stable.tar.xz"
 curl -L $FLUTTER_URL -o flutter.tar.xz
 tar -xf flutter.tar.xz
 rm flutter.tar.xz
