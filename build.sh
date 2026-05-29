d4b6efd9c0fcf152c808a667acb6efedb6b7bfad
Aakash Dodwal <naukariwalaav@gmail.com>
2026-05-29 13:44:29 +0530

    fixes

--- build.sh
+++ build.sh
@@ -4,12 +4,9 @@ set -e
 # Fix git dubious ownership issue
 git config --global --add safe.directory '*'
 
-# Download and install Flutter 3.29.0 (has Dart 3.9.0+)
+# Download and install the current Flutter stable release
 echo "Installing Flutter..."
-FLUTTER_URL="https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.29.0-stable.tar.xz"
-curl -L $FLUTTER_URL -o flutter.tar.xz
-tar -xf flutter.tar.xz
-rm flutter.tar.xz
+git clone --depth 1 --branch stable https://github.com/flutter/flutter.git flutter
 
 # Add Flutter to PATH
 export PATH="$PATH:$(pwd)/flutter/bin"
